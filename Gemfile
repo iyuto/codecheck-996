8ed9f046bbf4027943f88345f9f5d82da4faa934
Yuto Yazaki <130441151@ccmailg.meijo-u.ac.jp>
2016-02-17 15:39:43 +0900

    Install HighCharts
    HighChartsの道入。
    とりあえずFitbitのstepsをグラフにプロット。

--- Gemfile
+++ Gemfile
@@ -30,9 +30,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-# For Fitbit API
+# For Fitbit feature
 gem 'oauth2'
 gem 'json'
+gem 'lazy_high_charts'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/fitbit_controller.rb
+++ app/controllers/fitbit_controller.rb
@@ -7,7 +7,19 @@ class FitbitController < ApplicationController
   end
 
   def show
-    render json: @token.get_today_step
+    data = @token.get_today_step
+    category = data["activities-steps-intraday"]["dataset"].map {|item| item["time"][0,5] }
+    current_quantity = data["activities-steps-intraday"]["dataset"].map {|item| item["value"]}
+    steps = data["activities-steps-intraday"]["dataset"].map {|item| {y: item["value"], name: item["time"][0,5]}}
+    
+    # チャート生成
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: "歩数")
+      f.xAxis(categories: category, labels: {enabled: false})
+      f.yAxis(title: {text: nil}, labels: {enabled: false})
+      f.series(name: "歩", data: current_quantity, type: "column")
+      f.legend(enabled: false)
+    end
   end
   
   private
